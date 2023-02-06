7a406bda42ea7553faa1efc1f01a1c7722893e04
mchays12 <mchays12@gmail.com>
2023-02-06 13:47:12 -0500

    New App
    Possibly the wrong repository

---
+++ simple-js-app/js/scripts.js
@@ -0,0 +1,28 @@
+let pokemonTypes=[
+  'grass', 
+  'electric', 
+  'artificial'
+];
+
+let pokemonList = [
+  { pokemon1Height: 10,
+    pokemon1Name: 'Bulbasaur',
+    pokemon1Type: pokemonTypes [0]
+  },
+  {pokemon2Height: 8,
+    pokemon2Name: 'Pikachu',
+    pokemon2Type: pokemonTypes[1]
+  },
+  { pokemon3Height: 5,
+    pokemon3Name: 'Mewtwo',
+    pokemon3Type: pokemonTypes[2]
+  }
+];
+
+
+
+
+
+
+
+
